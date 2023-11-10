b2a9a54c5390f450d5bf794a5a39defd1c9b796a
Pablo <paablont@gmail.com>
2023-11-10 18:59:22 +0100

    Clase pelicula con getters y setters buenos

--- Peliculas.py
+++ Peliculas.py
@@ -1,11 +1,71 @@
-class Pelicula:
+class Pelicula :
     def __init__(self, titulo):
         self._titulo = titulo
+        self.puntuacion = None
         self._fecha_lanzamiento = None
         self._genero = None
         self._sinopsis = None
         self._director = None
         self._reparto = None
+
+    @property
+    def titulo(self):
+        return self._titulo
+
+    @property
+    def lanzamiento(self):
+        return self._fecha_lanzamiento
+
+    @property
+    def genero(self):
+        return self._genero
+
+    @property
+    def sinopsis(self):
+        return self._sinopsis
+
+    @property
+    def director(self):
+        return self._director
+
+    @property
+    def reparto(self):
+        return self._reparto
+
+    @property
+    def puntuacion(self):
+        return self._puntuacion
+
+    @property
+    def puntuacion(self):
+        return self._puntuacion
+
+    @puntuacion.setter
+    def puntuacion(self, puntuacion):
+        self._puntuacion = puntuacion
+
+    # Setters
+    @titulo.setter
+    def titulo(self, titulo):
+        self._titulo = titulo
+    @lanzamiento.setter
+    def lanzamiento(self, fecha_lanzamiento):
+        self._fecha_lanzamiento = fecha_lanzamiento
+    @genero.setter
+    def genero(self, genero):
+        self._genero = genero
+    @sinopsis.setter
+    def sinopsis(self, sinopsis):
+        self._sinopsis = sinopsis
+    @director.setter
+    def director(self, director):
+        self._director = director
+    @reparto.setter
+    def reparto(self, reparto):
+        self._reparto = reparto
+    @puntuacion.setter
+    def puntuacion(self, puntuacion):
+        self._puntuacion = puntuacion
 
     def __str__(self):
         return f"Título: {self._titulo}\n" \
@@ -17,46 +77,3 @@ class Pelicula:
                f"Puntuación: {self._puntuacion}"
 
 
-    # Getters
-    def get_titulo(self):
-        return self._titulo
-
-    def get_fecha_lanzamiento(self):
-        return self._fecha_lanzamiento
-
-    def get_genero(self):
-        return self._genero
-
-    def get_sinopsis(self):
-        return self._sinopsis
-
-    def get_director(self):
-        return self._director
-
-    def get_reparto(self):
-        return self._reparto
-
-    def get_puntuacion(self):
-        return self._puntuacion
-
-    # Setters
-    def set_titulo(self, titulo):
-        self._titulo = titulo
-
-    def set_fecha_lanzamiento(self, fecha_lanzamiento):
-        self._fecha_lanzamiento = fecha_lanzamiento
-
-    def set_genero(self, genero):
-        self._genero = genero
-
-    def set_sinopsis(self, sinopsis):
-        self._sinopsis = sinopsis
-
-    def set_director(self, director):
-        self._director = director
-
-    def set_reparto(self, reparto):
-        self._reparto = reparto
-
-    def set_puntuacion(self, puntuacion):
-        self._puntuacion = puntuacion
